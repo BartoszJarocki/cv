2d8529d9be689a4f0509a19de666006f26e8fc61
Bartosz Jarocki <bartosz.jarocki@hey.com>
2023-12-23 23:15:40 +0100

    Base work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,163 @@
-import Image from 'next/image'
+import { Inter } from 'next/font/google'
 
-export default function Home() {
+import ClevertechLogo from "../images/logos/clevertech.png";
+import JojoMobileLogo from "../images/logos/jojomobile.png";
+import NSNLogo from "../images/logos/nsn.svg";
+import ParabolLogo from "../images/logos/parabol.svg";
+import { Avatar, AvatarFallback, AvatarImage } from "../components/ui/avatar";
+import { Card, CardHeader, CardContent } from "../components/ui/card";
+import { Badge } from "../components/ui/badge";
+import Link from "next/link";
+
+const ResumeData = {
+  name: "Bartosz Jarocki",
+  email: "bartosz.jarocki@hey.com",
+  about: "I am a software engineer with a passion for building things.",
+  contact: {
+    email: "bartosz.jarocki@hey.com",
+    tel: "+48530213401",
+    social: [
+      {
+        name: "GitHub",
+        url: "https://github.com/BartoszJarocki",
+      },
+      {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/bjarocki/",
+      },
+      {
+        name: "Twitter",
+        url: "https://twitter.com/BartoszJarocki",
+      },
+    ],
+  },
+  education: [
+    {
+      school: "Wrocław University of Technology",
+      degree: "Bachelor's Degree in Control systems engineering and Robotics",
+      start: "2007",
+      end: "2010",
+    },
+  ],
+  work: [
+    {
+      company: "Parabol",
+      title: "Full Stack Developer",
+      logo: ParabolLogo,
+      start: "2021",
+      end: "Present",
+    },
+    {
+      company: "Clevertech",
+      title: "Lead Android Developer → Full Stack Developer",
+      logo: ClevertechLogo,
+      start: "2015",
+      end: "2021",
+    },
+    {
+      company: "Jojo Mobile",
+      title: "Android Developer → Lead Android Developer",
+      logo: JojoMobileLogo,
+      start: "2012",
+      end: "2015",
+    },
+    {
+      company: "Nokia Siemens Networks",
+      title: "C/C++ Developer",
+      logo: NSNLogo,
+      start: "2010",
+      end: "2012",
+    },
+  ],
+} as const;
+
+const getResumeData = async () => {
+  return ResumeData;
+};
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default async function Home() {
+  const resumeData = await getResumeData();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="container mx-auto p-6">
+      <section className="w-full md:w-3/4 mx-auto bg-white p-6 space-y-12">
+        <div className="flex items-center justify-between mb-6">
+          <div className="flex-1">
+            <h1 className="text-2xl font-bold">{resumeData.name}</h1>
+            <p className="text-gray-500">Software Engineer</p>
+          </div>
+
+          <Avatar className="h-24 w-24">
+            <AvatarImage alt={resumeData.name} src="/placeholder-avatar.jpg" />
+            <AvatarFallback>BJ</AvatarFallback>
+          </Avatar>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <section>
+          <h2 className="text-xl font-bold mb-4">Summary</h2>
+          <p className="text-gray-600">{resumeData.about}</p>
+        </section>
+        <section className="flex flex-col min-h-0 gap-y-5">
+          <h2 className="text-xl font-bold my-2">Work Experience</h2>
+          {resumeData.work.map((work) => {
+            return (
+              <Card key={work.company}>
+                <CardHeader>
+                  <h3 className="text-lg font-semibold leading-none">
+                    {work.company}
+                  </h3>
+                  <div className="text-gray-500 align-middle">
+                    {work.start} - {work.end}
+                  </div>
+                </CardHeader>
+                <CardContent className="mt-2">
+                  <p className="text-gray-600">{work.title}</p>
+                </CardContent>
+              </Card>
+            );
+          })}
+        </section>
+        <section>
+          <h2 className="text-xl font-bold mb-4">Education</h2>
+          {resumeData.education.map((education) => {
+            return (
+              <Card key={education.school}>
+                <CardHeader>
+                  <h3 className="text-lg font-semibold">{education.school}</h3>
+                  <p className="text-gray-500">
+                    {education.degree}, {education.start} - {education.end}
+                  </p>
+                </CardHeader>
+              </Card>
+            );
+          })}
+        </section>
+        <section>
+          <h2 className="text-xl font-bold mb-4">Skills</h2>
+          <div className="flex flex-wrap gap-2">
+            <Badge>JavaScript</Badge>
+            <Badge>React</Badge>
+            <Badge>Node.js</Badge>
+            <Badge>Angular</Badge>
+            <Badge>Git</Badge>
+          </div>
+        </section>
+        <section>
+          <h2 className="text-xl font-bold mb-4">Contact</h2>
+          <div className="flex flex-col gap-2">
+            <Link className="text-blue-500 hover:underline" href="#">
+              your-email@example.com
+            </Link>
+            <Link className="text-blue-500 hover:underline" href="#">
+              +1234567890
+            </Link>
+            <Link className="text-blue-500 hover:underline" href="#">
+              your-website.com
+            </Link>
+          </div>
+        </section>
+      </section>
     </main>
-  )
+  );
 }
