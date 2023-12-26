15ec5ebc63da730277892f5e410c81cba70b94dc
Bartosz Jarocki <bartosz.jarocki@hey.com>
2023-12-26 16:58:20 +0100

    Fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <main className="container relative mx-auto scroll-my-12 overflow-auto p-4 print:p-12 md:p-16">
-      <section className="mx-auto w-full max-w-2xl space-y-8 bg-white">
+      <section className="mx-auto w-full max-w-2xl space-y-8 print:space-y-6 bg-white">
         <div className="flex items-center justify-between">
           <div className="flex-1 space-y-1.5">
             <h1 className="text-2xl font-bold">{RESUME_DATA.name}</h1>
