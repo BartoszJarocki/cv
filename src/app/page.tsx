d153e18ea9cd20babd58db3449c3b01fb9c48bb3
Fermin Rodriguez Penelas <frodriguezpenelas@gmail.com>
2023-12-27 10:23:06 -0300

    meto tracking codes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -197,6 +197,9 @@ export default function Page() {
           })),
         ]}
       />
+    <script async src="https://cdn.splitbee.io/sb.js"></script>
+    <script async defer src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
+    <noscript><img src="https://queue.simpleanalyticscdn.com/noscript.gif" alt="" referrerpolicy="no-referrer-when-downgrade" /></noscript>
     </main>
   );
 }
