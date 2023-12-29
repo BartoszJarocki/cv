451837b76451a7a67c76318ed3b9a70b68f5cbb9
Bartosz Jarocki <bartosz.jarocki@hey.com>
2023-12-29 22:20:00 +0100

    Grammar fix

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -25,7 +25,7 @@ export const RESUME_DATA = {
   location: "Wrocław, Poland, CET",
   locationLink: "https://www.google.com/maps/place/Wrocław",
   about:
-    "Full Stack Engineer focused on building products with extra attention to details",
+    "Full Stack Engineer focused on building products with extra attention to detail",
   summary:
     "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
   avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
