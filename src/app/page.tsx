e41597bc5d23262536267d50263505a1d13b3291
Kartikey Chauhan <kartikeychauhan27@gmail.com>
2023-12-31 16:52:15 -0500

    feat: badges in education

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -145,8 +145,19 @@ export default function Page() {
               <Card key={education.school}>
                 <CardHeader>
                   <div className="flex items-center justify-between gap-x-2 text-base">
-                    <h3 className="font-semibold leading-none">
+                    <h3 className="inline-flex items-center justify-center gap-x-1 font-semibold leading-none">
                       {education.school}
+                      <span className="inline-flex gap-x-1">
+                        {education.badges.map((badge) => (
+                          <Badge
+                            variant="secondary"
+                            className="align-middle text-xs"
+                            key={badge}
+                          >
+                            {badge}
+                          </Badge>
+                        ))}
+                      </span>
                     </h3>
                     <div className="text-sm tabular-nums text-gray-500">
                       {education.start} - {education.end}
