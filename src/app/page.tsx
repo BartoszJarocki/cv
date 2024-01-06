0cf463b7a3cf1e103dbcaff7b504beac75142c78
Alexis Zugasti <alexiszugasti@gmail.com>
2024-01-05 19:21:56 -0600

    remove personal website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -170,10 +170,6 @@ export default function Page() {
 
       <CommandMenu
         links={[
-          {
-            url: RESUME_DATA.personalWebsiteUrl,
-            title: "Personal Website",
-          },
           ...RESUME_DATA.contact.social.map((socialMediaLink) => ({
             url: socialMediaLink.url,
             title: socialMediaLink.name,
