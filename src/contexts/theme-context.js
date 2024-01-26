ff046de52c1b39424a20dcde373b9d370693a931
Pedro Barreto <pbdeoliveira@gmail.com>
2024-01-26 12:44:42 -0300

    feat: theme context

---
+++ src/contexts/theme-context.js
@@ -0,0 +1,23 @@
+"use client";
+
+import { createContext, useContext, useState } from 'react';
+
+export const ThemeContext = createContext();
+
+export const useTheme = () => {
+  return useContext(ThemeContext);
+};
+
+export const ThemeProvider = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode((prevDarkMode) => !prevDarkMode);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ darkMode, toggleDarkMode}}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
