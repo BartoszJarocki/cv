d06b709894e12938a3ce85cb59aa04ad4fe858f2
Luis Mesajil <lhmesajil@gmail.com>
2024-02-21 21:16:13 -0500

    feat(resume): update profile data

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -9,7 +9,7 @@ export const RESUME_DATA = {
   about:
     "Full Stack Engineer prioritizing high-quality, scalable products with a focus on user experience.",
   summary:
-    "I'm currently pursuing a degree in computer engineering and have experience in fullstack projects using React and Node.js. My studies in computer science provide me with a strong foundation, complemented by my participation in employability accelerators where I've gained skills in innovative and agile development.",
+    "With a background in computer engineering, I bring extensive experience in developing both frontend and backend applications using technologies such as React, React Native, and Node.js. I'm well-versed in agile development frameworks like Scrum and Gitflow, ensuring efficient collaboration and code management throughout the development lifecycle. Across my projects, I've successfully implemented various functionalities including user authentication, chatbots, RESTful APIs, payment gateways, cron jobs, email notifications, and relational database development.",
   avatarUrl: "https://avatars.githubusercontent.com/u/45523084?v=4",
   personalWebsiteUrl: "",
   contact: {
@@ -49,15 +49,26 @@ export const RESUME_DATA = {
   ],
   work: [
     {
-      company: "Memory Kings",
-      link: "https://memorykings.pe/",
+      company: "Libellus",
+      link: "https://libellus.io/",
       badges: [],
-      title: "Payment Records Processor → Excel Macros Developer",
+      title: "Frontend Developer",
       logo: JojoMobileLogo,
-      start: "2021",
-      end: "2021",
+      start: "2024",
+      end: "2024",
       description:
-        "Programmed the final design for printing customer data in the home delivery product shipping process. Managed the processing of payment receipts, credit notes, and debit notes.",
+        "Crafted user interfaces, encompassing common components and screens. Utilized design guidelines from Figma and adhered to global design contexts. Developed user stories using Storybook for efficient testing and iteration.",
+    },
+    {
+      company: "Henry",
+      link: "https://www.soyhenry.com/",
+      badges: [],
+      title: "Full Stack Developer Graduate",
+      logo: JojoMobileLogo,
+      start: "2023",
+      end: "2023",
+      description:
+        "Engaged in various projects, including the creation of an ecommerce platform. Developed the API using Node.js and Express.js, incorporating combined filters, sorting functionalities, and relational database development for efficient data management. Implemented email notifications and ensured adherence to RESTful API standards. On the frontend, integrated third-party authentication, implemented a shopping cart with MercadoPago payment gateway, and designed a management dashboard for platform oversight. Additionally, integrated a chatbot utilizing the OpenAI API. Participated in daily group scrum meetings for project coordination and progress tracking.",
     },
     {
       company: "AprendeHoy",
@@ -85,13 +96,19 @@ export const RESUME_DATA = {
     {
       title: "DigiShoes",
       techStack: [
-        "Learning Project",
-        "Material UI",
         "React.js",
+        "Redux.js",
+        "Material UI",
+        "Node.js",
         "Express.js",
-        "Sequelize",
+        "RESTful API",
+        "MercadoPago",
+        "Email notifications",
+        "Firebase",
+        "Scrum",
       ],
-      description: "Ecommerce for footwear sales",
+      description:
+        "DigiShoes is an e-commerce platform specializing in footwear, offering a seamless shopping experience for customers. The platform incorporates essential features such as a shopping cart, email notifications, and user authentication to enhance user convenience and security.",
       logo: ConsultlyLogo,
       link: {
         label: "storecalzado.vercel.app",
@@ -101,50 +118,26 @@ export const RESUME_DATA = {
     {
       title: "GymSpace",
       techStack: [
-        "Learning Project",
-        "Material UI",
         "React.js",
+        "Redux.js",
+        "Material UI",
+        "Node.js",
         "Express.js",
-        "Sequelize",
+        "RESTful API",
+        "MercadoPago",
+        "Email notifications",
+        "Cron jobs",
+        "Auth0",
+        "Chatbot",
+        "OpenAI API",
+        "Scrum",
       ],
-      description: "Fitness Ecommerce and Gym Service Management Platform",
+      description:
+        "GymSpace is a dynamic e-commerce platform catering to fitness enthusiasts, offering a wide range of products and services to support their fitness journey. The platform seamlessly integrates essential features such as a shopping cart, email notifications, user authentication, and a chatbot for customer support, ensuring a streamlined and engaging user experience.",
       logo: ConsultlyLogo,
       link: {
         label: "github.com/LautaroEZM/GymSpaceFront",
         href: "https://github.com/LautaroEZM/GymSpaceFront",
-      },
-    },
-    {
-      title: "Food Recipes",
-      techStack: [
-        "Learning Project",
-        "CSS",
-        "React.js",
-        "Vite",
-        "Express.js",
-        "Sequelize",
-      ],
-      description: "Recipe Discovery and Favorites Manager",
-      logo: ConsultlyLogo,
-      link: {
-        label: "github.com/mesajil/food-henry-project",
-        href: "https://github.com/mesajil/food-henry-project",
-      },
-    },
-    {
-      title: "Rick and Morty",
-      techStack: [
-        "Learning Project",
-        "CSS",
-        "React.js",
-        "Express.js",
-        "Sequelize",
-      ],
-      description: "Rick and Morty Character Encyclopedia",
-      logo: ConsultlyLogo,
-      link: {
-        label: "github.com/mesajil/rick_and_morty",
-        href: "https://github.com/mesajil/rick_and_morty",
       },
     },
   ],
