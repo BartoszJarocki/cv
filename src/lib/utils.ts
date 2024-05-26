574ea3cbb46dba438d10330e7e27284ef20a9682
vibhaw raj <vibhawraj494@gmail.com>
2024-05-26 15:36:03 +0530

    [fixes]:#52

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
-import { type ClassValue, clsx } from "clsx";
+import { ClassValue } from "class-variance-authority/types";
 import { twMerge } from "tailwind-merge";
+import { cx } from "class-variance-authority";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(cx(inputs));
 }
