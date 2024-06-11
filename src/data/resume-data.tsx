2929872d2f968d53f81137b10215f274c4103650
Roselio Alejandro Pérez Trujillo <92204722+r911z@users.noreply.github.com>
2024-06-11 14:59:16 -0500

    Update names on resume-data.tsx
    first commit!

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -20,8 +20,8 @@ import {
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
+  name: "Roselio Alejandro Perez",
+  initials: "RAP",
   location: "Wrocław, Poland, CET",
   locationLink: "https://www.google.com/maps/place/Wrocław",
   about:
