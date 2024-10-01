dfa776500df6fe5ef82c9cb9e324b6e3a3a7ab02
ayamdobhal <me@iamdobhal.dev>
2024-10-01 22:23:53 +0530

    feat: use "ctrl" instead of command button for non mac clients

--- src/components/command-menu.tsx
+++ src/components/command-menu.tsx
@@ -11,8 +11,8 @@ import {
   CommandList,
   CommandSeparator,
 } from "@/components/ui/command";
-import { Button } from "./ui/button";
 import { CommandIcon } from "lucide-react";
+import { Button } from "./ui/button";
 
 interface Props {
   links: { url: string; title: string }[];
@@ -20,8 +20,11 @@ interface Props {
 
 export const CommandMenu = ({ links }: Props) => {
   const [open, setOpen] = React.useState(false);
+  const [isMac, setIsMac] = React.useState(false);
 
   React.useEffect(() => {
+    setIsMac(window.navigator.userAgent.indexOf("Mac") > -1);
+
     const down = (e: KeyboardEvent) => {
       if (e.key === "j" && (e.metaKey || e.ctrlKey)) {
         e.preventDefault();
@@ -38,7 +41,7 @@ export const CommandMenu = ({ links }: Props) => {
       <p className="fixed bottom-0 left-0 right-0 hidden border-t border-t-muted bg-white p-1 text-center text-sm text-muted-foreground print:hidden xl:block">
         Press{" "}
         <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
-          <span className="text-xs">⌘</span>J
+          <span className="text-xs">{isMac ? "⌘" : "Ctrl"}</span>+J
         </kbd>{" "}
         to open the command menu
       </p>
