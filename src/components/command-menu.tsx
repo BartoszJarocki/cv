47fe82a120fe8f8add4b9f38a6dc3044da3c3310
Ayam Dobhal <me@iamdobhal.dev>
2024-12-27 20:26:40 +0530

    feat: use "ctrl" instead of command button for non mac clients (#93)
    * feat: use "ctrl" instead of command button for non mac clients
    
    * fix: move `isMac` variable out of useEffect

--- src/components/command-menu.tsx
+++ src/components/command-menu.tsx
@@ -11,8 +11,8 @@ import {
   CommandList,
   CommandSeparator,
 } from "@/components/ui/command";
-import { Button } from "./ui/button";
 import { CommandIcon } from "lucide-react";
+import { Button } from "./ui/button";
 
 interface Props {
   links: { url: string; title: string }[];
@@ -20,6 +20,10 @@ interface Props {
 
 export const CommandMenu = ({ links }: Props) => {
   const [open, setOpen] = React.useState(false);
+  let isMac =
+    typeof window !== "undefined"
+      ? window.navigator.userAgent.indexOf("Mac") > -1
+      : false;
 
   React.useEffect(() => {
     const down = (e: KeyboardEvent) => {
@@ -35,10 +39,10 @@ export const CommandMenu = ({ links }: Props) => {
 
   return (
     <>
-      <p className="fixed bottom-0 left-0 right-0 hidden border-t border-t-muted bg-white p-1 text-center text-sm text-muted-foreground print:hidden xl:block">
+      <p className="fixed bottom-0 left-0 right-0 hidden border-t border-t-muted bg-white p-1 text-center text-sm text-muted-foreground xl:block print:hidden">
         Press{" "}
         <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
-          <span className="text-xs">⌘</span>J
+          <span className="text-xs">{isMac ? "⌘" : "Ctrl"}</span>+J
         </kbd>{" "}
         to open the command menu
       </p>
@@ -46,7 +50,7 @@ export const CommandMenu = ({ links }: Props) => {
         onClick={() => setOpen((open) => !open)}
         variant="outline"
         size="icon"
-        className="fixed bottom-4 right-4 flex rounded-full shadow-2xl print:hidden xl:hidden"
+        className="fixed bottom-4 right-4 flex rounded-full shadow-2xl xl:hidden print:hidden"
       >
         <CommandIcon className="my-6 size-6" />
       </Button>
