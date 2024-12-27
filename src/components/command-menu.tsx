b57206428edd4ab2f0ca19fbabffb54138eccdb7
ayamdobhal <me@iamdobhal.dev>
2024-12-27 06:23:01 +0530

    fix: move `isMac` variable out of useEffect

--- src/components/command-menu.tsx
+++ src/components/command-menu.tsx
@@ -20,11 +20,12 @@ interface Props {
 
 export const CommandMenu = ({ links }: Props) => {
   const [open, setOpen] = React.useState(false);
-  const [isMac, setIsMac] = React.useState(false);
+  let isMac =
+    typeof window !== "undefined"
+      ? window.navigator.userAgent.indexOf("Mac") > -1
+      : false;
 
   React.useEffect(() => {
-    setIsMac(window.navigator.userAgent.indexOf("Mac") > -1);
-
     const down = (e: KeyboardEvent) => {
       if (e.key === "j" && (e.metaKey || e.ctrlKey)) {
         e.preventDefault();
@@ -38,7 +39,7 @@ export const CommandMenu = ({ links }: Props) => {
 
   return (
     <>
-      <p className="fixed bottom-0 left-0 right-0 hidden border-t border-t-muted bg-white p-1 text-center text-sm text-muted-foreground print:hidden xl:block">
+      <p className="fixed bottom-0 left-0 right-0 hidden border-t border-t-muted bg-white p-1 text-center text-sm text-muted-foreground xl:block print:hidden">
         Press{" "}
         <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
           <span className="text-xs">{isMac ? "⌘" : "Ctrl"}</span>+J
@@ -49,7 +50,7 @@ export const CommandMenu = ({ links }: Props) => {
         onClick={() => setOpen((open) => !open)}
         variant="outline"
         size="icon"
-        className="fixed bottom-4 right-4 flex rounded-full shadow-2xl print:hidden xl:hidden"
+        className="fixed bottom-4 right-4 flex rounded-full shadow-2xl xl:hidden print:hidden"
       >
         <CommandIcon className="my-6 size-6" />
       </Button>
