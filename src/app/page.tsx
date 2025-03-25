84701a5f5d1351290dcb62c1900e2473f61b029c
Patrick Lehmann <p.lehmann@code-b.com>
2025-03-25 15:40:48 +0100

    feat: update locale and URLs in metadata for consistency and accuracy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,10 +15,12 @@ export const metadata: Metadata = {
     title: `${RESUME_DATA.name} - Resume`,
     description: RESUME_DATA.about,
     type: "profile",
-    locale: "en_US",
+    locale: "en_UK",
+    siteName: "Patrick Lehmann - Resume",
+    url: "https://cv.ptrcklehmann.com",
     images: [
       {
-        url: "https://ptrcklehmann.com/opengraph-image",
+        url: "https://cv.ptrcklehmann.com/opengraph-image",
         width: 1200,
         height: 630,
         alt: `${RESUME_DATA.name}'s profile picture`,
@@ -29,7 +31,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: `${RESUME_DATA.name} - Resume`,
     description: RESUME_DATA.about,
-    images: ["https://ptrcklehmann.com/opengraph-image"],
+    images: ["https://cv.ptrcklehmann.com/opengraph-image"],
   },
 };
 
